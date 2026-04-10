f7ad4e5c8f832853bcc082aa50751409a5d8f4f7
Jamie Chapman <104535858+bilbospocketses@users.noreply.github.com>
2026-04-10 10:59:08 -0400

    fix: remove phantom module references from Program.cs

--- src/ControlMenu/Program.cs
+++ src/ControlMenu/Program.cs
@@ -1,7 +1,6 @@
 using System.Reflection;
 using ControlMenu.Data;
 using ControlMenu.Modules;
-using ControlMenu.Modules.AndroidDevices.Services;
 using ControlMenu.Modules.Utilities.Services;
 using ControlMenu.Services;
 using Microsoft.AspNetCore.DataProtection;
@@ -31,9 +30,6 @@ builder.Services.AddScoped<ISecretStore, SecretStore>();
 builder.Services.AddScoped<IConfigurationService, ConfigurationService>();
 builder.Services.AddScoped<IDeviceService, DeviceService>();
 builder.Services.AddSingleton<INetworkDiscoveryService, NetworkDiscoveryService>();
-
-// Android Devices module services
-builder.Services.AddSingleton<IAdbService, AdbService>();
 
 // Utilities module services
 builder.Services.AddSingleton<IIconConversionService, IconConversionService>();
