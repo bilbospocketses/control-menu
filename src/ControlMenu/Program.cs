c383f81df913038d1c0bbba1eb5c8fc6bb81c543
Jamie Chapman <104535858+bilbospocketses@users.noreply.github.com>
2026-04-23 03:16:43 -0400

    feat(devices): register IDeviceTypeCache as scoped service

--- src/ControlMenu/Program.cs
+++ src/ControlMenu/Program.cs
@@ -49,6 +49,7 @@ builder.Services.AddSingleton<ICommandExecutor, CommandExecutor>();
 builder.Services.AddScoped<ISecretStore, SecretStore>();
 builder.Services.AddScoped<IConfigurationService, ConfigurationService>();
 builder.Services.AddScoped<IDeviceService, DeviceService>();
+builder.Services.AddScoped<IDeviceTypeCache, DeviceTypeCache>();
 builder.Services.AddScoped<IEmailService, EmailService>();
 builder.Services.AddSingleton<INetworkDiscoveryService, NetworkDiscoveryService>();
 
